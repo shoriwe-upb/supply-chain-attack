3d0c6b16243934a9bafffaca7a3a7d42c92edc95
Antonio <antoniojosedonishung@gmail.com>
2023-08-16 14:21:09 -0500

    ci: setup docker script

---
+++ setup-docker.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# This script is used to setup the actions runner
+
+# Install docker
+apt-get update
+apt-get install -y ca-certificates curl gnupg
+install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/debian/gpg | gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+chmod a+r /etc/apt/keyrings/docker.gpg
+echo \
+  "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian \
+  "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
+   tee /etc/apt/sources.list.d/docker.list > /dev/null
+apt-get update
+apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+# Create user
+/sbin/groupadd -g 5000 -r runner
+/sbin/useradd  -d /opt/runner -m -g runner -G docker -r -s /sbin/nologin -u 5000 runner
